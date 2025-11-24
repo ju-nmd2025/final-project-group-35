26c363e77a12d3fddaf7a4c5144d368a59751d24
olleaxelssson <hk8dxgs5n5@privaterelay.appleid.com>
2025-11-24 16:21:04 +0100

    Tweaked the gravity
    Tweaked the gravity so that the character now collides on all sides of the square

--- gravity.js
+++ gravity.js
@@ -10,6 +10,7 @@ export let gravity = {
     }
   },
   handlePlatformCollision(character, platform) {
+    // Downward collision (feet on platform)
     if (
       character.x + character.w > platform.x &&
       character.x < platform.x + platform.w &&
@@ -19,6 +20,39 @@ export let gravity = {
     ) {
       character.y = platform.y - character.h;
       character.vy = character.jumpStrength;
+    }
+    // Upward collision (head hits platform bottom)
+    if (
+      character.x + character.w > platform.x &&
+      character.x < platform.x + platform.w &&
+      character.vy < 0 &&
+      character.y <= platform.y + platform.h &&
+      character.y - character.vy > platform.y + platform.h
+    ) {
+      character.y = platform.y + platform.h;
+      character.vy = 0;
+    }
+    // Side collision (prevent moving through platform sides)
+    if (
+      character.y + character.h > platform.y &&
+      character.y < platform.y + platform.h
+    ) {
+      // Collided with left side
+      if (
+        character.x < platform.x + platform.w &&
+        character.x + character.w > platform.x &&
+        character.x < platform.x
+      ) {
+        character.x = platform.x - character.w;
+      }
+      // Collided with right side
+      if (
+        character.x < platform.x + platform.w &&
+        character.x + character.w > platform.x &&
+        character.x > platform.x
+      ) {
+        character.x = platform.x + platform.w;
+      }
     }
   },
 };
