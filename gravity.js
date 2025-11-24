d4d75cbc27158dedee7064db12771d8abf93e7cf
olleaxelssson <hk8dxgs5n5@privaterelay.appleid.com>
2025-11-24 14:57:32 +0100

    Added script for up and down gravity
    Created a script to hold the information about the gravity of the character

---
+++ gravity.js
@@ -0,0 +1,24 @@
+export let gravity = {
+  apply(character) {
+    character.vy += character.gravity;
+    character.y += character.vy;
+  },
+  handleFloorCollision(character, floorY) {
+    if (character.y + character.h >= floorY) {
+      character.y = floorY - character.h;
+      character.vy = character.jumpStrength;
+    }
+  },
+  handlePlatformCollision(character, platform) {
+    if (
+      character.x + character.w > platform.x &&
+      character.x < platform.x + platform.w &&
+      character.vy > 0 &&
+      character.y + character.h >= platform.y &&
+      character.y + character.h - character.vy < platform.y
+    ) {
+      character.y = platform.y - character.h;
+      character.vy = character.jumpStrength;
+    }
+  },
+};
