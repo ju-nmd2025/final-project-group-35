654e3ea06e8efce09811b4b9493fdfc6d3710eeb
olleaxelssson <hk8dxgs5n5@privaterelay.appleid.com>
2025-11-24 14:54:30 +0100

    Changes to shape and size
    Removed the ears to make the collisions easier. Changed the position values from hardcoded values to x and y values. Also added gravity and a check if the character is touching to ground to make it bounce or not.

--- character.js
+++ character.js
@@ -1,18 +1,24 @@
 export let character = {
+  x: 260,
+  y: 250,
+  w: 80,
+  h: 75,
+  vy: 0,
+  gravity: 0.6,
+  onGround: false,
+
   draw() {
     fill(200, 180, 0);
     strokeWeight(1);
-    rect(100 / 8, 100 / 8, 600 / 8, 600 / 8, 10);
-    arc(100 / 8, 100 / 8, 150 / 8, 150 / 8, HALF_PI, 2 * PI, OPEN);
-    arc(700 / 8, 100 / 8, 150 / 8, 150 / 8, PI, HALF_PI, OPEN);
+    rect(this.x, this.y, 600 / 8, 600 / 8, 10);
 
     fill(255);
-    circle(250 / 8, 250 / 8, 100 / 8);
-    circle(550 / 8, 250 / 8, 100 / 8);
-    arc(400 / 8, 450 / 8, 290 / 8, 280 / 8, 0, PI, PIE);
+    circle(this.x + 150 / 8, this.y + 150 / 8, 100 / 8);
+    circle(this.x + 450 / 8, this.y + 150 / 8, 100 / 8);
+    arc(this.x + 300 / 8, this.y + 250 / 8, 290 / 8, 280 / 8, 0, PI, PIE);
 
     fill(0);
-    circle(260 / 8, 240 / 8, 20 / 8);
-    circle(560 / 8, 240 / 8, 20 / 8);
+    circle(this.x + 160 / 8, this.y + 140 / 8, 20 / 8);
+    circle(this.x + 460 / 8, this.y + 140 / 8, 20 / 8);
   },
 };
