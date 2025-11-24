8df9d0dc2e844dd45ff652b063798562897e4b37
olleaxelssson <hk8dxgs5n5@privaterelay.appleid.com>
2025-11-24 14:56:44 +0100

    Created a script for sideways movement
    This script handels the sideways movement, and also that the character moves from left to right if it goes beyond the boundaries of the canvas

---
+++ movement.js
@@ -0,0 +1,17 @@
+export let movement = {
+  apply(character) {
+    if (keyIsDown(LEFT_ARROW)) {
+      character.x -= 10;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+      character.x += 10;
+    }
+    // Wrap around screen
+    if (character.x + character.w < 0) {
+      character.x = 600;
+    }
+    if (character.x > 600) {
+      character.x = -character.w;
+    }
+  },
+};
