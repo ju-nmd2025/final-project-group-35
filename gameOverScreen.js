e9e463f6c7a2ec4216108b00ab5ad873033e97e2
olleaxelssson <hk8dxgs5n5@privaterelay.appleid.com>
2025-11-25 11:35:17 +0100

    Added gameOver script
    Script that handles the game over screen by creating a let declaration

---
+++ gameOverScreen.js
@@ -0,0 +1,39 @@
+export let gameOverScreen = {
+  buttonX: 200,
+  buttonY: 370,
+  buttonW: 200,
+  buttonH: 60,
+
+  draw(width, height) {
+    // Draw game over screen
+    background(200, 50, 50);
+
+    // Draw game over text
+    textSize(48);
+    fill(255);
+    textAlign(CENTER, CENTER);
+    text("Game Over!", width / 2, height / 2 - 60);
+
+    // Draw restart button
+    fill(255);
+    rect(this.buttonX, this.buttonY, this.buttonW, this.buttonH);
+
+    // Draw button text
+    textSize(32);
+    fill(0);
+    text(
+      "Restart",
+      this.buttonX + this.buttonW / 2,
+      this.buttonY + this.buttonH / 2
+    );
+  },
+
+  isButtonClicked(mouseX, mouseY) {
+    return (
+      mouseX > this.buttonX &&
+      mouseX < this.buttonX + this.buttonW &&
+      mouseY > this.buttonY &&
+      mouseY < this.buttonY + this.buttonH
+    );
+  },
+};
